13be480c71502d77d34fdc4d365c17134ea218b2
BayuDC <dev.bayudc@gmail.com>
2022-08-13 07:21:37 +0700

    feat: watch member join

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import discord
 from config import *
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_member_join(member: discord.Member):
+    print(f"{member.display_name} joined")
+    pass
 
 
 @client.event
 async def on_ready():
     print('Discord bot is ready')
+
 
 client.run(token)
